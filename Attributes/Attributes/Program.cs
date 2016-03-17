a66c1e84e7e06a7a007bc25d71526b350b4a96a2
Shani Awan <Shani Awan>
2016-03-16 20:15:07 -0700

    Attributes

---
+++ Attributes/Attributes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new test();
+            t.MyProperty = false;
+            Console.WriteLine(t.MyProperty);
+            Console.ReadLine();
+        }
+    }
+    public class MyAttributes: Attribute
+    {
+        public int MyProperty { get; set; }
+        
+    }
+    [MyAttributes(MyProperty=10)]
+    public class test
+    {
+        public bool name= true;
+        public bool MyProperty {
+            
+            get
+            {
+                return name;
+            }
+            set { name=value; }
+        }
+    }
+}
