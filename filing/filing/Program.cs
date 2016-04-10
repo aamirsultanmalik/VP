644b699a00b5e4e47b8189eaa3093440f09586aa
aamir <shanimalik226@gmail.com>
2016-04-10 07:00:12 -0700

    filing practice

---
+++ filing/filing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileStream fs = new FileStream(@"C:\Users\Aamir Sultan Malik\Desktop\a.txt", FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+            //for (int i = 98; i < 127; i++)
+            //{
+            //    byte a = (byte)i;
+            //    fs.WriteByte(a);
+            //}
+            //for (int i = 0; i < 27; i++)
+            //{
+            //    char a = (char)fs.ReadByte();
+            //    Console.WriteLine(a);
+            //}
+            string[] str = File.ReadAllLines(@"C:\Users\Aamir Sultan Malik\Desktop\Program.cs");
+            File.WriteAllLines(@"C:\Users\Aamir Sultan Malik\Desktop\a.txt", str);
+        }
+    }
+}
